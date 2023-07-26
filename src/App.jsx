39021daa611b72a26ffd988a074048bbabb4b28c
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-07-26 06:53:51 +0500

    added-interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 import React from 'react'
+  const step = 3;
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='steps'>
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">Step {step}: {messages[step -1]}</p>
+      <div className="buttons">
+        <button style={{backgroundColor: "#7050f2", color: "#fff"}}>Previous</button>
+        <button style={{backgroundColor: "#7050f2", color: "#fff"}}>Next</button>
+
+      </div>
+    </div>
   )
 }
 
